12f2993caf2cdafacf9238ed1f0731c186de15c1
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-21 21:39:01 -0700

    Made topic overview page

--- app/src/main/java/edu/washington/bennyn/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package edu.washington.bennyn.quizdroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         val subjects = arrayOf("Math", "Physics", "Lego Marvel Superheroes")
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects)
         listView.adapter = adapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, topicOverview::class.java)
+            intent.putExtra("subject", subjects[position])
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/edu/washington/bennyn/quizdroid/topicOverview.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/topicOverview.kt
@@ -2,11 +2,26 @@ package edu.washington.bennyn.quizdroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class topicOverview : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic_overview)
+
+        val subjectName = findViewById<TextView>(R.id.subjectName)
+        val numQuestions = findViewById<TextView>(R.id.numQuestions)
+        val description = findViewById<TextView>(R.id.description)
+
+        val subject = intent.getStringExtra("subject")
+        val numOfQuestions = 3 //get from intent later
+        val details = "Description placeholder that will be initialized later!"
+        val numQuestionsText = "This topic contains $numOfQuestions questions"
+
+        subjectName.text = subject
+        description.text = details
+        numQuestions.text = numQuestionsText
+
     }
 }

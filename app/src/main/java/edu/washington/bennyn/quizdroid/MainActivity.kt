1e9b412c5d37fa327ff67b3201a67bcf8f3d7182
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-21 20:44:53 -0700

    Made listview of subjects

--- app/src/main/java/edu/washington/bennyn/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/MainActivity.kt
@@ -2,11 +2,19 @@ package edu.washington.bennyn.quizdroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+        val subjects = arrayOf("Math", "Physics", "Lego Marvel Superheroes")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects)
+        listView.adapter = adapter
     }
 }
---
+++ app/src/main/java/edu/washington/bennyn/quizdroid/topicOverview.kt
@@ -0,0 +1,12 @@
+package edu.washington.bennyn.quizdroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class topicOverview : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topic_overview)
+    }
+}

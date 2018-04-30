a14e5ca770595397d02cf0a2a5410cefb94033a7
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-30 13:47:10 -0700

    Fix quiz and topic objects, implement TopicRepository for offline usage, amd QuizApp into singleton

--- app/src/main/java/edu/washington/bennyn/quizdroid/QuizApp.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/QuizApp.kt
@@ -2,32 +2,15 @@ package edu.washington.bennyn.quizdroid
 
 import android.util.Log
 
-class QuizApp : android.app.Application(), TopicRepository {
+class QuizApp : android.app.Application() {
 
-    override var topics: Array<TopicObj>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-        set(value) {
-            for (i in 0..2) {
-                val topic = TopicObj("This is a title")
-                topic.longDesc = "long description"
-                topic.shortDesc = "short description"
-                for (i in 0..3) {
-                    val quiz = QuizObj("This is a question")
-                    quiz.answers = arrayOf("1", "2", "3", "4")
-                    quiz.answer = 1
-                    topic.questions[i] = quiz
-                }
-                topics[i] = topic
-            }
-        }
+    //Makes this into a singleton
+    companion object Repository {
+        val offlineRepository = offlineRepository()
+    }
 
     override fun onCreate() {
         super.onCreate()
         Log.d("appMessage", "QuizApp initialized!")
-    }
-
-    //So other classes can get the data from the TopicRepository
-    fun getTopic(index: Int): TopicObj {
-        return(topics[index])
     }
 }
--- app/src/main/java/edu/washington/bennyn/quizdroid/TopicRepository.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/TopicRepository.kt
@@ -1,17 +1,41 @@
 package edu.washington.bennyn.quizdroid
 
 interface TopicRepository {
-    var topics: Array<TopicObj> //hard-coded and stored in memory
+    var topics: Array<TopicObj>
+
+    fun getTopic(index: Int): TopicObj
+}
+
+//Implements the TopicRepository with hard-coded variables that are stored in memory
+class offlineRepository: TopicRepository {
+
+    override var topics: Array<TopicObj> = makeObjects()
+
+    //create the topics variable
+    private fun makeObjects(): Array<TopicObj> {
+        val topicList = mutableListOf<TopicObj>()
+        for (i in 0..2) {
+            val questionList = mutableListOf<QuizObj>()
+            for (i in 0..3) {
+                val question = QuizObj("This is a question", arrayOf("1", "2", "3", "4"), 1)
+                questionList.add(question)
+            }
+            val topic = TopicObj("This is a title", "shortDesc", "longDesc",
+                                 questionList.toTypedArray())
+            topicList.add(topic)
+        }
+        return (topicList.toTypedArray())
+    }
+
+    //get the topic from a topic array by its index
+    override fun getTopic(index: Int): TopicObj {
+        return (topics[index])
+    }
 }
 
 //This object is technically a "Question" object as well
-data class QuizObj(val question: String) {
-    lateinit var answers: Array<String>
-    var answer = 1 //default answer
-}
+data class QuizObj(val question: String, val answers: Array<String>, val answer: Int)
 
-data class TopicObj(val title: String) {
-    lateinit var shortDesc: String
-    lateinit var longDesc: String
-    lateinit var questions: Array<QuizObj>
-}
+//Object for a topic that carries multiple questions
+data class TopicObj(val title: String, val shortDesc: String, val longDesc: String,
+                    val questions: Array<QuizObj>)

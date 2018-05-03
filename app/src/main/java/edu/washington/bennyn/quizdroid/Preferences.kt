a7abc62c2ffd3b9d631f6027bf4b8eb852a0ba2f
Benny Nguyen <benny.nguyenx@gmail.com>
2018-05-02 21:36:15 -0700

    Finish settings page

--- app/src/main/java/edu/washington/bennyn/quizdroid/Preferences.kt
+++ app/src/main/java/edu/washington/bennyn/quizdroid/Preferences.kt
@@ -2,6 +2,9 @@ package edu.washington.bennyn.quizdroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.TextView
 
 class Preferences : AppCompatActivity() {
 
@@ -10,5 +13,20 @@ class Preferences : AppCompatActivity() {
         setContentView(R.layout.activity_preferences)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val urlView = findViewById<TextView>(R.id.urlInput)
+        val urlSubmitBtn = findViewById<Button>(R.id.urlSubmitButton)
+        val numberPicker = findViewById<NumberPicker>(R.id.numberPicker)
+        numberPicker.maxValue = 10
+        numberPicker.minValue = 0
+        numberPicker.wrapSelectorWheel = false
+
+
+        urlSubmitBtn.setOnClickListener {
+            //Implement later
+        }
+        numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+            //Implement later
+        }
     }
 }
